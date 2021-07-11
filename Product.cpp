14c12fe8bd03c1bc0411a614e1328a444ef6d229
Qasba <82276586+Qasba@users.noreply.github.com>
2021-07-11 20:34:50 +0530

    Add files via upload

---
+++ Product.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ using namespace std;
+
+int main() {
+    int x,y;
+    int multiply;
+    cout << "Enter 1st number: ";
+    cin >> x;
+    cout << "Enter 2nd number: ";
+    cin >> y;
+    multiply =x*y;
+    cout << " The product of two numbers is" <<multiply; 
+    return 0;
+ }
